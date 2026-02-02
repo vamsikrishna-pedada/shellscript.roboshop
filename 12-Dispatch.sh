8efc9609a2e5489e76f44ad64b925ac6527d5f20
Vamsi <vamsikrishnapedada19@gmail.com>
2026-02-02 15:15:53 +0530

    dispatch Configuration Script

---
+++ 12-Dispatch.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+
+dnf install golang -y &>>$LOGS_FILE
+VALIDATE $? "Installed golang"
+
+id roboshop &>>$LOGS_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+    VALIDATE $? "Created system user"
+else
+    echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+fi
+
+mkdir -p /app 
+VALIDATE $? "Created app directory"
+
+curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip   &>>$LOGS_FILE
+VALIDATE $? "Downloading Dispatch code"
+
+cd /app
+VALIDATE $? "Moving to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
+unzip /tmp/dispatch.zip &>>$LOGS_FILE
+VALIDATE $? "Unzip Dispatch code"
+
+go mod init dispatch &>>$LOGS_FILE
+VALIDATE $? "Installed packages from go mod init"
+
+go get  &>>$LOGS_FILE
+VALIDATE $? "Installed packages from go get"
+
+go build  &>>$LOGS_FILE
+VALIDATE $? "Installed packages from go build"
+
+
+cp $SCRIPT_DIR/dispatch.service /etc/systemd/system/dispatch.service
+VALIDATE $? "Created systemctl service"
+
+systemctl daemon-reload
+systemctl enable dispatch  &>>$LOGS_FILE
+systemctl start dispatch
+VALIDATE $? "Enabled and Started Dispatch"
