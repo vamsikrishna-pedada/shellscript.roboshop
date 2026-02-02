cd1443ded4f3be68cb2867589c616a9175dcd6bb
Vamsi <vamsikrishnapedada19@gmail.com>
2026-02-02 14:29:30 +0530

    Redis Configuration Script

---
+++ 05-Redis.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable redis -y &>>$LOGS_FILE
+VALIDATE $? "Disabled Default Redis Version"
+
+dnf module enable redis:7 -y &>>$LOGS_FILE
+VALIDATE $? "Enabled Redis 7 Version"
+
+dnf install redis -y &>>$LOGS_FILE
+VALIDATE $? "Installed Redis"
+
+sed -i '87s/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf &>>$LOGS_FILE
+VALIDATE $? "Allowing the remote connections"
+
+sed -i '111s/yes/no/g' /etc/redis/redis.conf &>>$LOGS_FILE
+VALIDATE $? "protected-mode from yes to no configuration"
+
+systemctl enable redis &>>$LOGS_FILE
+VALIDATE $? "Enabled Redis"
+
+systemctl start redis &>>$LOGS_FILE
+VALIDATE $? "Started Redis"
